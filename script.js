5488a642dd1ceba4ee90666a7983ca77888076ce
RiyadTangil <riyadkhanxp2@gmail.com>
2021-01-26 23:25:40 +0600

    plus button added

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const plusBtn = document.getElementById('plusBtn');
 plusBtn.addEventListener("click",function(){
-    
+ const plusBtnValue =  document.getElementById('plusBtnInput').value;
+const plusBtnParseValue = parseFloat(plusBtnValue);
+document.getElementById('plusBtnInput').value = plusBtnParseValue+1;
+
 })
